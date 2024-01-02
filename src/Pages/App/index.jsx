fc91ebe53c739191faa5c5606880862c445b1153
Tatiana <tati14953@gmail.com>
2024-01-01 22:20:28 -0300

    Routes protection improved

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -12,32 +12,47 @@ import './App.css'
 import { useContext, useEffect } from 'react'
 
 const AppRoutes = () => {
-  const context = useContext(ShoppingCartContext)
-  const isUserSignOut = context.signOut
-  console.log("sign out ", isUserSignOut);
-  const parsedAccount = context.account || {};
+  const context = useContext(ShoppingCartContext);
+  const account = localStorage.getItem('account');
+  const parsedAccount = account ? JSON.parse(account) : null;
+  const hasAnAccount = !!parsedAccount && Object.keys(parsedAccount).length > 0;
+  const isUserSignOut = context.signOut;
 
-  // Comprobamos si hay un usuario autenticado
-  const isAuthenticated = Object.keys(parsedAccount).length > 0;
-  console.log("parsed account ", parsedAccount);
-  console.log("si esta autentificado: ", isAuthenticated);
-  let routes = useRoutes([
-    { path: '/', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/clothes', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/electronics', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/furnitures', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/toys', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/others', element: isAuthenticated ? <Home /> : <SignOut /> },
-    { path: '/my-account', element: isUserSignOut ? <SignOut/> : <MyAccount /> },
-    { path: '/my-order', element: isUserSignOut ? <SignOut/> : <MyOrder /> },
-    { path: '/my-orders', element: isUserSignOut ? <SignOut/> : <MyOrders /> },
-    { path: '/my-orders/last', element: isUserSignOut ? <SignOut/> : <MyOrder /> },
-    { path: '/my-orders/:id', element: <MyOrder /> },
-    { path: '/sign-in', element: <SignOut /> },
-    { path: '/*', element: <NotFound /> },
-  ])
+  let routes = [];
 
-  return routes
+  // Si el usuario está desconectado y no tiene cuenta, mostrar opciones para crear una cuenta o iniciar sesión
+  if (!hasAnAccount && !isUserSignOut) {
+    routes = [
+      { path: '/', element: <SignOut /> },
+      { path: '/sign-in', element: <SignOut /> },
+      { path: '/clothes', element: <SignOut /> },
+      { path: '/electronics', element:<SignOut /> },
+      { path: '/furnitures', element: <SignOut /> },
+      { path: '/toys', element: <SignOut /> },
+      { path: '/others', element: <SignOut /> },
+    ];
+  } else {
+    // Si el usuario tiene una cuenta o ha iniciado sesión
+    routes = [
+      { path: '/', element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/clothes', element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/electronics', element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/furnitures',element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/toys', element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/others', element: !isUserSignOut ? <Home /> : <SignOut /> },
+      { path: '/my-account', element: <MyAccount /> },
+      { path: '/my-order', element: <MyOrder /> },
+      { path: '/my-orders', element: <MyOrders /> },
+      { path: '/my-orders/last', element: <MyOrder /> },
+      { path: '/my-orders/:id', element: <MyOrder /> },
+      { path: '/sign-in', element: <SignOut /> },
+      { path: '/*', element: <NotFound /> },
+    ];
+  }
+
+  const routing = useRoutes(routes);
+
+  return routing;
 }
 
 const App = () => {
@@ -47,7 +62,6 @@ const App = () => {
     if (account) {
       const parsedAccount = JSON.parse(account);
       context && context.setAccount && context.setAccount(parsedAccount);
-      console.log(parsedAccount);
     }
   }, [context]);
 
